c48b0e9f9b571b7926771e376237174280c12b80
Ubuntu <ubuntu@ip-172-31-37-95.ap-northeast-2.compute.internal>
2024-06-05 09:08:56 +0000

    -0605 박준규 로그인 기능 구현

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-const express=require('express');
+const express=require('express'); //module import
 const app=express() // express 실행
 
 app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
     console.log('서버 시작');
+}) 
+
+app.get('/',(req,res)=>{
+    console.log('루트에 대한 요청');
+    res.send('루트페이지에 대한 요청');
+}) 
+
+app.get('/test',(req,res)=>{
+    console.log('===> test에 대한 요청');
+    res.send('test에 대한 요청');
+})
+
+app.get('/main',(req,res)=>{
+    console.log('main에 대한 요청');
+    res.sendFile(__dirname+'/pages/index.html');
+}) 
+
+app.get('/login',(req,res)=>{
+    console.log('로그인 요청');
+    res.send('로그인 페이지 예시');
 }) 
